0f0f7d66ea3053697d0e73fa99e73799c99c68fe
Rifky Manuel Satyana <rmscoaldev@gmail.com>
2023-10-04 00:44:47 +0700

    chore: installed material tailwind and try components

--- frontend/src/components/position/Center.js
+++ frontend/src/components/position/Center.js
@@ -1,3 +1,3 @@
 export default function Center({ children, className }) {
-  return <div className={`flex justify-center place-content-center ${className}`}>{children}</div>;
+  return <div className={`flex justify-center place-content-center items-center ${className}`}>{children}</div>;
 }
--- frontend/src/view/Login/Login.js
+++ frontend/src/view/Login/Login.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
-import BlackBackground from '../../components/body/BlackBackground';
 import Center from '../../components/position/Center';
+
+import { Button, Card, CardBody, CardFooter, CardHeader, Input, Typography } from '@material-tailwind/react';
 
 const initForm = {
   username: '',
@@ -11,10 +12,29 @@ export default function LoginView() {
   const [form, setForm] = useState(initForm);
 
   return (
-    <BlackBackground className="h-screen">
-      <Center className="h-full">
-        <button value={form.username}>Login</button>
-      </Center>
-    </BlackBackground>
+    <Center className="h-screen w-screen">
+      <Card className="w-96 h-max">
+        <CardHeader variant="gradient" color="gray" className="mb-4 grid h-28 place-items-center">
+          <Typography variant="h3" color="white">
+            Sign In
+          </Typography>
+        </CardHeader>
+        <CardBody className="flex flex-col gap-4">
+          <Input label="Email" size="lg" />
+          <Input label="Password" size="lg" />
+        </CardBody>
+        <CardFooter className="pt-0">
+          <Button variant="gradient" fullWidth>
+            Sign In
+          </Button>
+          <Typography variant="small" className="mt-6 flex justify-center">
+            Don&apos;t have an account?
+            <Typography as="a" href="#signup" variant="small" color="blue-gray" className="ml-1 font-bold">
+              Sign up
+            </Typography>
+          </Typography>
+        </CardFooter>
+      </Card>
+    </Center>
   );
 }
